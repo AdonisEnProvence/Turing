6e1230baeefe2a42325103a36a7900a0e4f4b64f
prastoin <tkesray@gmail.com>
2022-05-20 11:20:06 +0200

    feat: define conventionnal logs for each tape process
    Co-authored-by: Baptiste Devessier <baptiste@devessier.fr>

--- src/interpreter.erl
+++ src/interpreter.erl
@@ -27,16 +27,38 @@ move_index_on_tape({Index, Tape, right}) ->
             {RightIndex, Tape}
     end.
 
+print_tape_and_head_on_tape(IndexOnTape, Tape) ->
+    io:format("Tape: ["),
+    print_tape_and_head_on_tape(IndexOnTape, Tape, 1),
+    io:format("]~n").
+
+print_tape_and_head_on_tape(IndexOnTape, Tape, CurrentIndexOnTape) ->
+    IndexOnTapeIsCurrentIndex = IndexOnTape =:= CurrentIndexOnTape,
+    TapeCurrentValue = lists:nth(CurrentIndexOnTape, Tape),
+    CurrentIndexOnTapeIsLastIndex = CurrentIndexOnTape =:= length(Tape),
+
+    if 
+        IndexOnTapeIsCurrentIndex ->
+            io:format("<~p>", [TapeCurrentValue]);
+        true ->
+            io:format("~p", [TapeCurrentValue])
+    end,
+    if 
+        CurrentIndexOnTapeIsLastIndex ->
+            ok;
+        true ->
+            io:format(","),
+            print_tape_and_head_on_tape(IndexOnTape, Tape, CurrentIndexOnTape + 1)
+    end.
+
 read_and_exec(IndexOnTape, Tape, MachineConfig) ->
-    io:format("Read and execute index tape value ...~n"),
+    print_tape_and_head_on_tape(IndexOnTape, Tape),
     TapeCurrentValue = lists:nth(IndexOnTape, Tape),
     if
         TapeCurrentValue =:= "0" ->
-            io:format("TapeCurrentValue == 0~n", []),
             {NewIndex, NewTape} = move_index_on_tape({IndexOnTape, Tape, left}),
             read_and_exec(NewIndex, NewTape, MachineConfig);
         TapeCurrentValue =:= "1" ->
-            io:format("TapeCurrentValue == 1~n", []),
             {NewIndex, NewTape} = move_index_on_tape({IndexOnTape, Tape, right}),
             read_and_exec(NewIndex, NewTape, MachineConfig);
         true ->
