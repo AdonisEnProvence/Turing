3ae51c60857bf6cc22a9c927020c88720eea13bf
prastoin <tkesray@gmail.com>
2022-05-31 11:07:51 +0200

    feat: machine validate blank & tests

--- src/machine_validator.erl
+++ src/machine_validator.erl
@@ -1,10 +1,13 @@
 -module(machine_validator).
 
 -ifdef(TEST).
--export([validate_machine_alphabet/1, validate_machine_states/1]).
+-export([validate_machine_alphabet/1, validate_machine_states/1, validate_machine_blank/2]).
 -else.
 -export([]).
 -endif.
+
+% Reminder validation step is after parser step
+% We then assume that the received data is already parsed
 
 look_for_duplicated_in_list(List) ->
     ListLength = erlang:length(List),
@@ -19,6 +22,13 @@ look_for_duplicated_in_list(List) ->
             {error, {duplicated_characters, DuplicatedCharacterList}}
     end.
 
+is_alphabet_character(Character, Alphabet) ->
+    CharacterIsInAlphabet = lists:member(Character, Alphabet),
+    if
+        CharacterIsInAlphabet =:= true -> ok;
+        CharacterIsInAlphabet =:= false -> {error, {expected_alphabet_character, Character}}
+    end.
+
 % Alphabet validation
 validate_machine_alphabet(Alphabet) ->
     look_for_duplicated_in_list(Alphabet).
@@ -26,3 +36,7 @@ validate_machine_alphabet(Alphabet) ->
 % States validation
 validate_machine_states(States) ->
     look_for_duplicated_in_list(States).
+
+% Blank Validation
+validate_machine_blank(Blank, Alphabet) ->
+    is_alphabet_character(Blank, Alphabet).
--- test/machine_validator_test.erl
+++ test/machine_validator_test.erl
@@ -2,54 +2,51 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+get_valid_alphabet() ->
+    [
+        "1",
+        ".",
+        "-",
+        "="
+    ].
+
+get_valid_states() ->
+    [
+        "scanright",
+        "eraseone",
+        "subone",
+        "skip",
+        "HALT"
+    ].
+
 % Validate Alphabet
 validate_machine_alphabet_test() ->
-    ok = machine_validator:validate_machine_alphabet([
-        "1",
-        ".",
-        "-",
-        "="
-    ]).
+    ok = machine_validator:validate_machine_alphabet(get_valid_alphabet()).
 parse_machine_alphabet_duplicated_entry_test() ->
-    {error, {duplicated_characters, ["="]}} = machine_validator:validate_machine_alphabet([
-        "1",
-        ".",
-        "=",
-        "-",
-        "="
-    ]).
+    {error, {duplicated_characters, ["="]}} = machine_validator:validate_machine_alphabet(
+        get_valid_alphabet() ++ ["="]
+    ).
 parse_machine_alphabet_several_duplicated_entry_test() ->
-    {error, {duplicated_characters, [".", "="]}} = machine_validator:validate_machine_alphabet([
-        "1",
-        ".",
-        "=",
-        ".",
-        "-",
-        "="
-    ]).
+    {error, {duplicated_characters, [".", "="]}} = machine_validator:validate_machine_alphabet(
+        get_valid_alphabet() ++ [".", "="]
+    ).
 
 % Validate States
 validate_machine_states_test() ->
-    ok = machine_validator:validate_machine_states([
-        "1",
-        ".",
-        "-",
-        "="
-    ]).
+    ok = machine_validator:validate_machine_states(get_valid_states()).
 parse_machine_states_duplicated_entry_test() ->
-    {error, {duplicated_characters, ["="]}} = machine_validator:validate_machine_states([
-        "1",
-        ".",
-        "=",
-        "-",
-        "="
-    ]).
+    {error, {duplicated_characters, ["HALT"]}} = machine_validator:validate_machine_states(
+        get_valid_states() ++ ["HALT"]
+    ).
 parse_machine_states_several_duplicated_entry_test() ->
-    {error, {duplicated_characters, ["/", "+"]}} = machine_validator:validate_machine_states([
-        "1",
-        "/",
-        "+",
-        "/",
-        "-",
-        "+"
-    ]).
+    {error, {duplicated_characters, ["HALT", "scanright"]}} = machine_validator:validate_machine_states(
+        get_valid_states() ++ ["HALT", "scanright"]
+    ).
+
+% Validate Blank
+validate_machine_blank_test() ->
+    ok = machine_validator:validate_machine_blank(".", get_valid_alphabet()).
+validate_machine_blank_not_alphabet_character_error_test() ->
+    {error, {expected_alphabet_character, "$"}} = machine_validator:validate_machine_blank(
+        "$", get_valid_alphabet()
+    ).
