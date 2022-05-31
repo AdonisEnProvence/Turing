a96f3091d7d7bd8c372db3b79fbf54650cef6331
prastoin <tkesray@gmail.com>
2022-05-31 09:54:59 +0200

    chore: auto format files

--- src/turing.erl
+++ src/turing.erl
@@ -13,15 +13,17 @@
 %%====================================================================
 
 format_program_usage() ->
-    io:format("usage: ft_turing [-h] jsonfile input
-
-positional arguments:
-  jsonfile          json description of the machine
-
-  input             input of the machine
-
-optional arguments:
-  -h, --help        show this help message and exit~n").
+    io:format(
+        "usage: ft_turing [-h] jsonfile input\n"
+        "\n"
+        "positional arguments:\n"
+        "  jsonfile          json description of the machine\n"
+        "\n"
+        "  input             input of the machine\n"
+        "\n"
+        "optional arguments:\n"
+        "  -h, --help        show this help message and exit~n"
+    ).
 
 format_read_file_error(Reason) ->
     io:format("Error while reading machine configuration json file: ~p~n", [Reason]).
@@ -33,7 +35,9 @@ format_try_decode_error({Reason, _Stack}) ->
 parse_optionnal_first_flag_arg([]) ->
     format_program_usage(),
     {error, empty_args};
-parse_optionnal_first_flag_arg([FirstArg | _OtherArgs]) when FirstArg =:= "--help"; FirstArg =:= "-h" ->
+parse_optionnal_first_flag_arg([FirstArg | _OtherArgs]) when
+    FirstArg =:= "--help"; FirstArg =:= "-h"
+->
     format_program_usage(),
     exit;
 parse_optionnal_first_flag_arg(Args) when length(Args) =:= 2 ->
@@ -41,7 +45,6 @@ parse_optionnal_first_flag_arg(Args) when length(Args) =:= 2 ->
 parse_optionnal_first_flag_arg(_) ->
     format_program_usage(),
     {error, too_many_args}.
-
 
 get_raw_machine_config([FilePath, _Input]) ->
     ReadFileResult = file:read_file(FilePath),
@@ -54,16 +57,16 @@ get_raw_machine_config([FilePath, _Input]) ->
 
 decode_raw_machine_config(BinaryFile) ->
     TryDecodeBinaryFileResult = jsone:try_decode(BinaryFile),
-    case TryDecodeBinaryFileResult of 
+    case TryDecodeBinaryFileResult of
         {ok, DecodedBinaryFile, _} ->
             parse_decoded_machine_config(DecodedBinaryFile);
         {error, Error} ->
             format_try_decode_error(Error)
     end.
 
-parse_decoded_machine_config(DecodedMachineConfig) -> 
+parse_decoded_machine_config(DecodedMachineConfig) ->
     ParsedMachineResult = parser:parse_machine(DecodedMachineConfig),
-    case ParsedMachineResult of 
+    case ParsedMachineResult of
         {error, _Error} ->
             % parser:format_error(error, Error);
             io:format("Parser error");
@@ -72,7 +75,7 @@ parse_decoded_machine_config(DecodedMachineConfig) ->
             start_machine(ParsedMachineConfig)
     end.
 % validate_parsed_machine_config(ParsedMachineConfig)->
-% parse_and_validate_word( DecodedMachineConfig) -> 
+% parse_and_validate_word( DecodedMachineConfig) ->
 start_machine(ParsedMachineConfig) ->
     interpreter:start(ParsedMachineConfig, ["1", "1", "1", "-", "1", "1", "="]).
 
