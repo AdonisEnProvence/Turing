b7251f285370dd8ea3f0621ede01e7deac1ba65c
prastoin <tkesray@gmail.com>
2022-06-09 06:00:58 +0200

    refactor: avoid iterating seperatly and state error construction

--- our-machines/generate_utm_machine_config.mjs
+++ our-machines/generate_utm_machine_config.mjs
@@ -21,6 +21,7 @@ const GO_TO_INPUT_START_FOR_PREFIX = "go-to-input-start-for_";
 const READ_TAPE_FOR_PREFIX = "read-tape-for_";
 const SCANLEFT_TO_STATES_DECLERATION_FOR =
   "scanleft-to-states-decleration-for_"; // scanleft-to-states-decleration-for_S(0)
+const FIND_STATE_PREFIX = "find-state_";
 
 const STATIC_ALPHABET = [
   STATE_DECLERATION_START,
@@ -60,11 +61,9 @@ if (!readWriteDuplicationReservedError) {
   process.exit(1);
 }
 
-const updatedAlphabet = [
-  ...STATIC_ALPHABET,
-  ...configStates,
-  ...configReadWrite,
-];
+const dynamicAlphabet = [...configStates, ...configReadWrite];
+const updatedAlphabet = [...STATIC_ALPHABET, ...dynamicAlphabet];
+const customConfigInputFindableCharCollection = [BLANK, ...configReadWrite];
 const customConfigStatesWithHalt = [HALT_STATE, ...configStates];
 const customConfigStates = configStates;
 console.log(updatedAlphabet);
@@ -104,26 +103,25 @@ customConfigStatesWithHalt.forEach((state) => {
 });
 
 // Then build "go-to-input-start-for_S",
-const goToInputStartForStateNames = customConfigStates.map(
-  (state) => GO_TO_INPUT_START_FOR_PREFIX + state
-);
-result.states = [...result.states, ...goToInputStartForStateNames];
-goToInputStartForStateNames.forEach((state) => {
-  result.transitions[state] = [];
+customConfigStates.forEach((configState) => {
+  const newState = GO_TO_INPUT_START_FOR_PREFIX + configState;
+  result.states.push(newState);
+  result.transitions[newState] = [];
+
   updatedAlphabet.forEach((character) => {
     if (character === INPUT_DECLERATION_START) {
-      result.transitions[state].push({
+      result.transitions[newState].push({
         read: character,
-        to_state: READ_TAPE_FOR_PREFIX + state,
+        to_state: READ_TAPE_FOR_PREFIX + configState,
         write: character,
         action: ACTION_RIGHT,
       });
       return;
     }
 
-    result.transitions[state].push({
+    result.transitions[newState].push({
       read: character,
-      to_state: state,
+      to_state: newState,
       write: character,
       action: ACTION_RIGHT,
     });
@@ -131,20 +129,49 @@ goToInputStartForStateNames.forEach((state) => {
 });
 
 // Then build "read-tape-for_CHAR"
-const ReadTapeForStateNames = customConfigStates.map(
-  (state) => READ_TAPE_FOR_PREFIX + state
-);
-result.states = [...result.states, ...ReadTapeForStateNames];
+for (const configState of customConfigStates) {
+  const newState = READ_TAPE_FOR_PREFIX + configState;
+  result.states.push(newState);
+  result.transitions[newState] = [];
 
-for (const state of ReadTapeForStateNames) {
-  result.transitions[state] = [];
-  for (const character of configReadWrite) {
-    result.transitions[state].push({
-      read: character,
-      to_state: `${SCANLEFT_TO_STATES_DECLERATION_FOR}${state}(${character})`,
+  for (const inputCharacter of customConfigInputFindableCharCollection) {
+    result.transitions[newState].push({
+      read: inputCharacter,
+      to_state: `${SCANLEFT_TO_STATES_DECLERATION_FOR}${configState}(${inputCharacter})`,
       write: CURRENT_READ_CHARACTER,
       action: ACTION_LEFT,
     });
+  }
+}
+
+// Then build "scanleft-to-states-decleration-for_S(0)"
+for (const state of customConfigStates) {
+  for (const inputCharacter of customConfigInputFindableCharCollection) {
+    const newState = `${SCANLEFT_TO_STATES_DECLERATION_FOR}${state}(${inputCharacter})`;
+    result.states.push(newState);
+    result.transitions[newState] = [];
+
+    for (const alphabetCharacter of updatedAlphabet) {
+      switch (alphabetCharacter) {
+        case INITIAL_STATE_DECLERATION_END: {
+          result.transitions[newState].push({
+            read: INITIAL_STATE_DECLERATION_END,
+            to_state: `${FIND_STATE_PREFIX}${state}(${inputCharacter})`,
+            write: INITIAL_STATE_DECLERATION_END,
+            action: ACTION_RIGHT,
+          });
+          break;
+        }
+        default: {
+          result.transitions[newState].push({
+            read: alphabetCharacter,
+            to_state: newState,
+            write: alphabetCharacter,
+            action: ACTION_LEFT,
+          });
+        }
+      }
+    }
   }
 }
 
