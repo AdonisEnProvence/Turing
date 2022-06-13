dfedfa786755cf7da72ee164533f9946b19c7378
prastoin <tkesray@gmail.com>
2022-06-13 10:39:56 +0200

    Revert "refactor: put color on current tape head"
    This reverts commit cc486587e0e6d1a55d5e388d20bb940cf474af68.

--- src/interpreter.erl
+++ src/interpreter.erl
@@ -83,7 +83,7 @@ print_head_index_value_on_tape(IndexOnTape, Tape, CurrentIndexOnTape) ->
 
     if
         IndexOnTapeIsCurrentIndex ->
-            io:format("\033[0;101m~s\033[0m", [TapeCurrentValue]);
+            io:format("<~s>", [TapeCurrentValue]);
         true ->
             io:format("~s", [TapeCurrentValue])
     end,
